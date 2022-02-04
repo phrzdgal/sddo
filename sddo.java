4635dd44b088477d9b1543976c535e5e965e8467
phrzdgal <phiroze.khattra@gmail.com>
2022-02-04 10:32:36 -0800

    libs created, failing test created

--- sddo.java
+++ sddo.java
@@ -1,2 +1,5 @@
 public class sddo {
+    public static int subtraction(int n1, int n2) {
+        return n1 - n2; 
+    }
 }
---
+++ sddotest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class sddotest { 
+    @Test
+    public void subtraction() {
+        assertEquals(2, sddo.subtraction(5, 1));
+    }
+}
